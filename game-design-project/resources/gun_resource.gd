3239b3851115396ea6f944fc6fd6971aee27a9e3
MFreit50 <michaelfreitas.mf50@gmail.com>
2024-09-24 17:33:48 -0400

    Update gun_resource.gd
    restructured variable order and added setters to timers

--- game-design-project/resources/gun_resource.gd
+++ game-design-project/resources/gun_resource.gd
@@ -2,6 +2,12 @@ extends Resource
 class_name Gun_Resource
 
 const max = 2000000000
+
+var fire_rate_timer : Timer = Timer.new()
+var reload_speed_timer : Timer = Timer.new()
+var burst_delay_timer : Timer = Timer.new()
+var charge_time_timer : Timer = Timer.new()
+
 @export var weapon_name : String
 @export var activate_animation : String
 @export var shoot_animation : String
@@ -9,34 +15,43 @@ const max = 2000000000
 @export var deactivate_animation : String
 @export var out_of_ammo_animation : String
 
+@export_range(0, max) var ammo_ammount : int					#ammo ammount in gun
+@export_range(0, max) var ammo_reserve : int					#ammo available for reloading
+@export_range(0, max) var magazine_size: int					#amout of ammo to reload
+@export_range(0, max) var max_ammo : int						#max ammo amount allowed at reserve
+@export var automatic_fire : bool								#if true weapons fire by holding shoot button
+
 @export var bullet_scene : PackedScene
-@export var ammo_ammount : int				#ammo ammount in gun
-@export var ammo_reserve : int				#ammo available for reloading
-@export var magazine_size: int				#amout of ammo to reload
-@export var max_ammo : int					#max ammo amount allowed at reserve
-@export var automatic_fire : bool			#if true weapons fire by holding shoot button
+@export var damage : float										#damage the gun deals
+@export_range(0, 1) var accuracy : float						#how accurate the gun is in percentage
+@export_range(0, max) var bullet_count : int					#how many bullets should be fired from one shot
+@export_range(0,360) var bullet_spread : float
+@export_range(0, max) var bullet_speed : float
+@export_range(0, max) var tile_damage : int
+@export_range(0, max) var tile_penetration : int				#how many tiles the bullet can penetrate before disappearing
+@export_range(0, max) var enemy_penetration : int				#how many enemies the bullet can penetrate before disappearing
 
 #timed variables
-@export_range(0, 300) var reload_speed : float			#how long it takes to reload
-@export_range(0, 300) var fire_rate : float				#how fast the gun shoots in seconds
-@export_range(0, 300) var charge_time : float				#how long the weapon needs to charge before shot
-@export_range(0, 300) var burst_delay : float				#delay between bullets in one shot
-
-@export var bullet_count : int				#how many bullets should be fired from one shot
-@export_range(0,360) var bullet_spread : float
-
-
-@export var damage : float					#damage the gun deals
-@export var bullet_speed : float
-@export var tile_penetration : int			#how many tiles the bullet can penetrate before disappearing
-@export var tile_damage : int
-@export var enemy_penetration : int			#how many enemies the bullet can penetrate before disappearing
-@export_range(0,1) var accuracy : float		#how accurate the gun is in percentage
-
-var fire_rate_timer : Timer = Timer.new()
-var reload_speed_timer : Timer = Timer.new()
-var burst_delay_timer : Timer = Timer.new()
-
+@export_range(0, 300) var fire_rate : float:					#how fast the gun shoots in seconds
+	set(value):
+		fire_rate = value
+		fire_rate_timer.set_wait_time(max(fire_rate, 0.00001))
+		
+@export_range(0, 300) var burst_delay : float:					#delay between bullets in one shot
+	set(value):
+		burst_delay = value
+		burst_delay_timer.set_wait_time(max(burst_delay, 0.00001))
+		
+@export_range(0, 300) var reload_speed : float:					#how long it takes to reload
+	set(value):
+		reload_speed = value
+		reload_speed_timer.set_wait_time(max(reload_speed, 0.00001))
+			
+@export_range(0, 300) var charge_time : float:					#how long the weapon needs to charge before shot
+	set(value):
+		charge_time = value
+		charge_time_timer.set_wait_time(max(charge_time, 0.00001))
+		
 func initialize() -> void:
 	'''
 	must be called by the script using this resource
