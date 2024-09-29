b660119c80533dc3e35be699a2519d6e44964e45
MFreit50 <michaelfreitas.mf50@gmail.com>
2024-09-29 18:53:48 -0400

    Update trail.gd
    made changes to stop debug warnings

--- game-design-project/misc/particles/trail/trail.gd
+++ game-design-project/misc/particles/trail/trail.gd
@@ -17,7 +17,7 @@ func _ready() -> void:
 	set_gradient_point(0, gradient_start_color)
 	set_gradient_point(1, gradient_end_color)
 	
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	queue.push_front(_get_position())
 	if queue.size() > MAX_LENGTH:
 		queue.pop_back()
@@ -28,7 +28,7 @@ func _process(delta: float) -> void:
 func _get_position():
 	return get_parent().global_position
 
-func set_gradient_point(point : float, color : Color) -> void:
+func set_gradient_point(point : int, color : Color) -> void:
 	'''"
 	this method is for setting the color at the index of a point that has 
 	already been created, by default, point 0 and 1 already exist, so setting
